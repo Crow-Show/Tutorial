3814559736ab7c6d6c366f22cc0e8ed41d1c2f9c
CrowShow <crow.show2109@gmail.com>
2025-11-28 15:13:42 +0300

    Feat: topic 2.2 is closed

---
+++ Topic_2_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+Console.InputEncoding = Encoding.UTF8;                      //подключает кирилицу
+
+const string President = "Путин";                           //мне казалось что константа гравитации тут не смотрится
+
+int userAge = 23;
+string userName = "Олег";
+string userCite = "Москва";
+
+Console.WriteLine($@"==============================
+* Имя пользователя:{userName}      *
+* Возвраст пользователя: {userAge}  *
+* Город пользователя: {userCite} *
+* Президент: {President}           *
+==============================");
+
+Console.ReadKey();
