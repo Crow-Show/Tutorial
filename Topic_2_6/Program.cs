ae7f66c20d848c96d4aff16810918482ce32373c
CrowShow <crow.show2109@gmail.com>
2025-11-29 10:11:45 +0300

    Feat: topic 2.6 is closed

---
+++ Topic_2_6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+Console.InputEncoding = Encoding.UTF8;
+
+Console.Write("Введите число секунд: ");            // уточнение про секунды подразумевает ввод положительного числа
+bool result = uint.TryParse(Console.ReadLine(), out uint input);        // uint для отсечения отрицательных чисел
+
+uint day = input / 86400;
+uint hour = (input % 86400) / 3600;
+uint minute = (input % 3600) / 60;
+uint second = input % 60;
+
+Console.Clear();
+
+Console.WriteLine($@"Исходное время: {input} секунд
+
+Итоговое время:
+Дни: {day}
+Часы:{hour}
+Минуты: {minute}
+Секунды: {second}");
+
+Console.ReadKey();
