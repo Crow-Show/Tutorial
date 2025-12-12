18116366f4b493805abb2ccb54894ab0dc645e84
CrowShow <crow.show2109@gmail.com>
2025-12-12 14:14:50 +0300

    review fixes

--- Topic_2_3/Program.cs
+++ Topic_2_3/Program.cs
@@ -7,14 +7,14 @@ string name = "Олег";
 int age = 18;
 char initial = 'О';
 bool isRegistred = true;
-const double PI = 3.14159;
+const double pi = 3.14159;
 
 Console.WriteLine($@"---------------------
 Имя: {name}
 Возраст: {age}
 Инициал: {initial}
 Зарегистрирован: {isRegistred}
-Число PI: {PI}
+Число PI: {pi}
 ---------------------");
 
 Console.ReadKey();
