076c755c4cca69d6ff4618ce607d4a0217bb5a85
CrowShow <crow.show2109@gmail.com>
2025-12-28 02:49:36 +0300

    fix reviev

--- Topic_2_6/Program.cs
+++ Topic_2_6/Program.cs
@@ -4,7 +4,7 @@ Console.OutputEncoding = Encoding.UTF8;
 Console.InputEncoding = Encoding.UTF8;
 
 Console.Write("Введите число секунд: ");            // уточнение про секунды подразумевает ввод положительного числа
-bool result = uint.TryParse(Console.ReadLine(), out uint input);        // uint для отсечения отрицательных чисел
+uint.TryParse(Console.ReadLine(), out uint input);        // uint для отсечения отрицательных чисел
 
 uint day = input / 86400;
 uint hour = (input % 86400) / 3600;
