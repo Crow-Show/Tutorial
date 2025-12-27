4656c00616de52a9175569ab15ac414a0c7077a5
CrowShow <crow.show2109@gmail.com>
2025-12-28 02:46:38 +0300

    fix reviev

--- Topic_2_5/Program.cs
+++ Topic_2_5/Program.cs
@@ -4,7 +4,7 @@ Console.OutputEncoding = Encoding.UTF8;
 Console.InputEncoding = Encoding.UTF8;
 
 string? name;
-string? cite;
+string? city;
 int age;
 double number;
 
@@ -12,7 +12,7 @@ Console.Write("Введите своё имя: ");
 name = Console.ReadLine();
 
 Console.Write("Введите свой город: ");
-cite = Console.ReadLine();
+city = Console.ReadLine();
 
 Console.Write("Введите свой возраст: ");
 bool result1 = int.TryParse(Console.ReadLine(), out age);           
@@ -22,11 +22,11 @@ bool result2 = double.TryParse(Console.ReadLine(), out number);
 
 Console.Clear();
 
-Console.WriteLine(@"=====================");   //смысла выводить вебратим строкой только рамку никакой,
+Console.WriteLine("=====================");   //смысла выводить вебратим строкой только рамку никакой,
 Console.WriteLine($"Имя: {name}");             //так бы я все ей сделал. зачем болше комманд, когда можно меньше комманд
 Console.WriteLine($"Возврат: {age}");
 Console.WriteLine($"Число: {number}");
-Console.WriteLine($"Город: {cite}");
+Console.WriteLine($"Город: {city}");
 Console.WriteLine(@"=====================");
 
 Console.ReadKey();
